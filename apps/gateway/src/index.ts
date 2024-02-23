898514c5ccc367b4004fc4224dba87016ee48b85
bitshiftmod <bitshiftmod@gmail.com>
2024-02-22 20:55:10 -0500

    experimenting with websockets

--- apps/gateway/src/index.ts
+++ apps/gateway/src/index.ts
@@ -1,27 +1,47 @@
 import express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
-import servers from "./nodes.json";
+import WebSocket from 'ws';
 
 const app = express();
 
+// interface OrangeNode  {
+//   name: string;
+//   ws: WebSocket;
+// }
+const connectedNodes:Array<WebSocket> = [];
+
 const selectNode = () => {
-  const index = Math.floor(Math.random() * servers.length);
-  console.log("Selected node: ", servers[index].name, servers[index].url);
-  return servers[index].url;
+  const index = Math.floor(Math.random() * connectedNodes.length);
+  const node = connectedNodes[index]
+  console.log("Selected node " + index);
+  return node;
 };
 
 /* ROUTES */
-app.use("/", (req, res, next) => {
-  createProxyMiddleware({
-    target: selectNode(),
-    changeOrigin: true,
-    onProxyReq: (proxyReq, req, res) => {
-      // TODO: replace with scheme to verify requests come for orange gateways
-      proxyReq.setHeader("x-orange-server","testing");
-    },
-  })(req, res, next);
+app.get("*", (req, res) => {
+  if(connectedNodes.length > 0) {
+    const node = selectNode();
+    node.send(JSON.stringify({
+      headers: req.headers,
+      query: req.query,
+      params: req.params,
+      body: req.body
+    }));
+  }
+  res.send("Hello World");
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT || 3000);
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+const WS_PORT = Number(process.env.WS_PORT || 3001);
+const wss = new WebSocket.Server({port: WS_PORT});
+wss.on('connection', (ws) => {
+  console.log('New connection');
+  const entry = ws;
+  connectedNodes.push(ws);
+  ws.on('close', () => {
+    console.log('Connection closed');
+    connectedNodes.splice(connectedNodes.indexOf(ws), 1);
+  });
+});
--- apps/orange-node/src/index.ts
+++ apps/orange-node/src/index.ts
@@ -1,41 +1,53 @@
-import express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import WebSocket from "ws";
 
-const app = express();
+// import express from "express";
+// import { createProxyMiddleware } from "http-proxy-middleware";
 
-// TODO: replace with scheme to verify requests come from orange gateways
-const isAccessAllowed = (req:express.Request) => {
-  return req.headers["x-orange-server"] === "testing";
-}
+// const app = express();
 
-// Custom middleware to intercept all calls
-app.use((req, res, next) => {
-  if (!isAccessAllowed(req)) {
-    res.status(403).send("Forbidden");
-  } else {
-    // remove API key from request
-    delete req.headers["x-orange-server"];
-    // console.log(`Incoming request for ${req.url}`);
-    // console.log(`headers: ${JSON.stringify(req.headers)}`);
-    next(); // Proceed to the next middleware/route handler
-  }
+// // TODO: replace with scheme to verify requests come from orange gateways
+// const isAccessAllowed = (req:express.Request) => {
+//   return req.headers["x-orange-server"] === "testing";
+// }
+
+// // Custom middleware to intercept all calls
+// app.use((req, res, next) => {
+//   if (!isAccessAllowed(req)) {
+//     res.status(403).send("Forbidden");
+//   } else {
+//     // remove API key from request
+//     delete req.headers["x-orange-server"];
+//     // console.log(`Incoming request for ${req.url}`);
+//     // console.log(`headers: ${JSON.stringify(req.headers)}`);
+//     next(); // Proceed to the next middleware/route handler
+//   }
+// });
+
+// // default to A1CN node
+// const ALGOD_URL = process.env.ALGOD_URL || "http://localhost:4160";
+
+// /* ROUTES */
+// app.use(
+//   "/",
+//   createProxyMiddleware({
+//     target: ALGOD_URL,
+//     changeOrigin: true,
+//     // onProxyReq: (proxyReq, req, res) => {
+//     //   console.log("Headers: " + JSON.stringify(req.headers));
+//     // }
+//   })
+// );
+
+// // Start the server
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
+const ws = new WebSocket("ws://localhost:3001");
+
+ws.on("open", () => {
+  ws.on("message", (data) => {
+    const req = JSON.parse(data.toString());
+    console.log("Data received: ", JSON.stringify(req));
+  });
 });
-
-// default to A1CN node
-const ALGOD_URL = process.env.ALGOD_URL || "http://localhost:4160";
-
-/* ROUTES */
-app.use(
-  "/",
-  createProxyMiddleware({
-    target: ALGOD_URL,
-    changeOrigin: true,
-    // onProxyReq: (proxyReq, req, res) => {
-    //   console.log("Headers: " + JSON.stringify(req.headers));
-    // }
-  })
-);
-
-// Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
