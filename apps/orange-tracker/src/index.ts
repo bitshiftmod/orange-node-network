d54550bc5c8ca6b973978c25807eb18616d4f4bf
bitshiftmod <bitshiftmod@gmail.com>
2024-02-25 12:03:02 -0500

    chore: switch to nodemon and adjust ports for better dev experience

--- apps/orange-tracker/src/index.ts
+++ apps/orange-tracker/src/index.ts
@@ -14,5 +14,5 @@ app.use(
 );
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
